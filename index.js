bc459823563a1b3351b688dd168be5db9d5161cb
imon hossin <imonhossin104@gmail.com>
2021-10-31 22:06:08 +0600

    first

--- index.js
+++ index.js
@@ -15,46 +15,112 @@ const uri =` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run (){
     try{
+
+
         await client.connect() ;
         console.log('database connected')
         const database = client.db('waterBoom')
       const servicesCollection = database.collection('services')
-    //  GET API 
-    app.get('/services',async(req,res)=>{
-        const cursor  = servicesCollection.find({}) 
-        const services = await cursor.toArray() 
-        res.send(services)
-    })
-    // POST API 
-        app.post('/services', async(req,res)=>{
-            const service = req.body ;
-            const result = await servicesCollection.insertOne(service)
-          console.log('hit the api',service)
-          res.json(result)
-       
-        })
-      // Single Service / add Order 
-       app.post("/services", (req,res)=>{
-        console.log(req.body)
-        servicesCollection.insertOne(req.body).then(result=>{
-          res.send(result)
-        })
-       })
-      //  Get My Order 
-      app.get("/myOrder/:email",async(req,res)=>{
-        console.log(req.params.email)
-        const result = await servicesCollection.find({email : req.params.email}).toArray()
-        res.send(result)
-      })
-    // My Order Delete API 
-      app.delete('/services/:id',async(req,res)=>{
-      const id = req.params.id 
-      const query = {_id:ObjectId(id)} 
+      const orderCollection = database.collection("order")
 
-      const result = await servicesCollection.deleteOne(query)
-      console.log('deleting user with id',result) 
-      res.json(result)
-      })
+    //  GET API 
+    // app.get('/services',async(req,res)=>{
+    //     const cursor  = servicesCollection.find({}) 
+    //     const services = await cursor.toArray() 
+    //     res.send(services)
+    // })
+    // // POST API 
+    //     app.post('/services', async(req,res)=>{
+    //         const service = req.body ;
+    //         const result = await servicesCollection.insertOne(service)
+    //       console.log('hit the api',service)
+    //       res.json(result)
+       
+    //     })
+    //   // Single Service / add Order 
+    //    app.post("/services", (req,res)=>{
+    //     console.log(req.body)
+    //     servicesCollection.insertOne(req.body).then(result=>{
+    //       res.send(result)
+    //     })
+    //    })
+    //   //  Get My Order 
+    //   app.get("/myOrder/:email",async(req,res)=>{
+    //     console.log(req.params.email)
+    //     const result = await servicesCollection.find({email : req.params.email}).toArray()
+    //     res.send(result)
+    //   })
+    // // My Order Delete API 
+    //   app.delete('/services/:id',async(req,res)=>{
+    //   const id = req.params.id 
+    //   const query = {_id:ObjectId(id)} 
+
+    //   const result = await servicesCollection.deleteOne(query)
+    //   console.log('deleting user with id',result) 
+    //   res.json(result)
+    //   })
+// ---------------------------------------------------------------------------------------------------------------------- // 
+// ----------------------------------------------- New Code ------------------------------------------------//
+// ---------------------------------------------------------------------------------------------------------------------- // 
+
+// get api
+app.get('/services', async (req, res) => {
+    const cursor = servicesCollection.find({});
+    const plans = await cursor.toArray();
+    res.send(plans);
+})
+// get api by id
+app.get('/services/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const query = { _id: ObjectId(id) }
+    const result = await servicesCollection.findOne(query);
+    res.send(result)
+})
+
+//post api
+app.post('/services', async (req, res) => {
+    const tours = req.body;
+    console.log(tours)
+    console.log(res)
+    const result = await servicesCollection.insertOne(tours);
+    res.json(result);
+})
+
+// add orders api
+app.post('/orders', async (req, res) => {
+    const order = req.body;
+    console.log('order', order);
+    const result = await orderCollection.insertOne(order);
+    res.json(result)
+})
+
+app.get('/orders', async (req, res) => {
+    const cursor = orderCollection.find({});
+    const plans = await cursor.toArray();
+    res.send(plans);
+})
+
+app.get('/orders/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const query = { _id: ObjectId(id) }
+    const result = await orderCollection.findOne(query);
+    res.send(result)
+})
+// delete api by id
+app.delete('/orders/:id', async (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const query = { _id: ObjectId(id) }
+    const result = await orderCollection.deleteOne(query);
+    res.send(result)
+})
+
+// ---------------------------------------End Code ---------------------------------//
+// ---------------------------------------------------------------------------------------------------------------------- // 
+// ---------------------------------------------------------------------------------------------------------------------- // 
+
     }
     finally{
         // await client.close();
@@ -69,3 +135,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Running Server From Port',port)
 })
+
+
+
+
+
+       
