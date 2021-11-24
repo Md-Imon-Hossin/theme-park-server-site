5628f4e0f286996124c4d6054c7381a6f8346c54
imon hossin <imonhossin104@gmail.com>
2021-11-24 21:57:56 +0600

    first

--- index.js
+++ index.js
@@ -23,45 +23,7 @@ async function run (){
       const servicesCollection = database.collection('services')
       const orderCollection = database.collection("order")
 
-    //  GET API 
-    // app.get('/services',async(req,res)=>{
-    //     const cursor  = servicesCollection.find({}) 
-    //     const services = await cursor.toArray() 
-    //     res.send(services)
-    // })
-    // // POST API 
-    //     app.post('/services', async(req,res)=>{
-    //         const service = req.body ;
-    //         const result = await servicesCollection.insertOne(service)
-    //       console.log('hit the api',service)
-    //       res.json(result)
-       
-    //     })
-    //   // Single Service / add Order 
-    //    app.post("/services", (req,res)=>{
-    //     console.log(req.body)
-    //     servicesCollection.insertOne(req.body).then(result=>{
-    //       res.send(result)
-    //     })
-    //    })
-    //   //  Get My Order 
-    //   app.get("/myOrder/:email",async(req,res)=>{
-    //     console.log(req.params.email)
-    //     const result = await servicesCollection.find({email : req.params.email}).toArray()
-    //     res.send(result)
-    //   })
-    // // My Order Delete API 
-    //   app.delete('/services/:id',async(req,res)=>{
-    //   const id = req.params.id 
-    //   const query = {_id:ObjectId(id)} 
-
-    //   const result = await servicesCollection.deleteOne(query)
-    //   console.log('deleting user with id',result) 
-    //   res.json(result)
-    //   })
-// ---------------------------------------------------------------------------------------------------------------------- // 
-// ----------------------------------------------- New Code ------------------------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------- // 
+   
 
 // get api
 app.get('/services', async (req, res) => {
@@ -117,9 +79,7 @@ app.delete('/orders/:id', async (req, res) => {
     res.send(result)
 })
 
-// ---------------------------------------End Code ---------------------------------//
-// ---------------------------------------------------------------------------------------------------------------------- // 
-// ---------------------------------------------------------------------------------------------------------------------- // 
+
 
     }
     finally{
@@ -136,7 +96,6 @@ app.listen(port, () => {
     console.log('Running Server From Port',port)
 })
 
-// ===================
 
 
 
